eafe890e185362927815fd747ce755a885ae0a39
ALUNO <ALUNO@DESKTOP-GBBOQ6M>
2022-02-18 16:28:32 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Quantos numeros irá colocar?");
+            string[] strNumeros = Console.ReadLine().Split(' ');
+            
+            for (int i = 0; i < strNumeros.Length; i++)
+            {
+            //    strNumeros[i] = Convert.ToInt32(strNumeros[i]);
+            }
+
+            int[] numeros = new int[10];
+            
+            bool certo;
+            for (int s = 0; s < numeros.Length; s++)
+            {
+                Console.WriteLine("Digite os numeros " );
+
+            volta1:
+                
+                certo = int.TryParse(Console.ReadLine(), out numeros[s]);
+                Console.Clear();
+                if  (certo == false)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Opção inválida, digite novamente");
+                    Console.ResetColor();
+                    goto volta1;
+                }
+
+
+
+            }
+            Array.Sort(numeros);
+            int cont;
+            int num;
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                cont = 0;
+                for (int j = 0; j < numeros.Length; j++)
+                {
+                    if (numeros[i] == numeros[j])
+                        cont++;
+                    num = numeros[i];
+
+
+                    Console.WriteLine($" repeticoes numero {num}: {cont} vezes");
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ConsoleApp3000/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ConsoleApp3000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vezesfalado;
+            char olhando = 'N' ;
+            string direcao;
+            do
+            {
+                Console.WriteLine("numero de comandos emitidos");
+                vezesfalado = int.Parse(Console.ReadLine());
+                if (vezesfalado == 0)
+                {
+                    break;
+                }
+            volta:
+                Console.WriteLine("Direçoes E ou D(maiusculo de preferencia)");
+
+                direcao = Console.ReadLine();
+
+                char[] instrucao = direcao.ToUpper().ToCharArray(); ;
+
+                for (int i = 0; i < instrucao.Length; i++)
+                {
+                    if (instrucao[i] != 'E' && instrucao[i] != 'D')
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Deu algum erro, digite novamente");
+                        Console.ResetColor();
+                        goto volta;
+                    }
+                    else
+                    {
+                        if (instrucao[i] == 'E')
+                        {
+                            switch (olhando)
+                            {
+                                case 'N':
+                                    olhando = 'O';
+                                    break;
+
+                                case 'O':
+                                    olhando = 'S';
+                                    break;
+
+                                case 'S':
+                                    olhando = 'L';
+                                    break;
+
+                                case 'L':
+                                    olhando = 'N';
+                                    break;
+                            }
+                        }
+                        else if (instrucao[i] == 'D')
+                        {
+                            switch (olhando)
+                            {
+                                case 'N':
+                                    olhando = 'L';
+                                    break;
+
+                                case 'L':
+                                    olhando = 'S';
+                                    break;
+
+                                case 'S':
+                                    olhando = 'O';
+                                    break;
+
+                                case 'O':
+                                    olhando = 'N';
+                                    break;
+                            }
+
+                        }
+                    }
+
+
+
+
+
+
+                }
+                Console.WriteLine($"A direção que estará apontada será {olhando}");
+                Console.ReadKey();
+            } while (true);
+
+
+
+
+
+        }
+    }
+}
